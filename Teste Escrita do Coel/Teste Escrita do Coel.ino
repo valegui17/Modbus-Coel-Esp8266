62af90049ae1b2bb28ea55b941eef7ce62c4f547
Guilherme Vale <valegui17@gmail.com>
2022-10-14 07:39:40 -0300

    Atualizado 13/10/2022

--- Teste Escrita do Coel/Teste Escrita do Coel.ino
+++ Teste Escrita do Coel/Teste Escrita do Coel.ino
@@ -7,27 +7,60 @@ float Setpoint = 5;
 word setpoint = 5.00;
 float Compressor = 0;
 float Porta = 0;
+uint8_t j, result;
 
 
 void setup() {
   Serial.begin(9600);
   node.begin(1, Serial);
-
 }
 
 void loop() {
 
-  static uint32_t i;
-  uint8_t j, result;
-  uint16_t data[6];
-  
-  // set word 1 of TX buffer to most-significant word of counter (bits 31..16)
-  node.setTransmitBuffer(0x2803, 0x5);
-  Serial.println(node.setTransmitBuffer(0x2803, 0x5));
-  delay(1000);
-  
-  // slave: write TX buffer to (2) 16-bit registers starting at register 0
-  result = node.writeSingleRegister(0x2803, 0x5);
+// ESCREVER VALOR DO SETPOINT
+  result = node.writeSingleRegister(0x2803, word(30));
+  Serial.println(" ");
+  Serial.println("SETPOINT");
   Serial.println(result);
-  delay(5000);
+  delay(1000);
+
+
+// RESETAR VALOR DA TEMPERATURA MAXIMA
+  result = node.writeSingleRegister(0x287, word(1));
+  Serial.println(" ");
+  Serial.println("RESET MAXIMO HT");
+  Serial.println(result);
+  delay(1000);
+
+
+// RESETAR VALOR DA TEMPERATURA MINIMA
+  result = node.writeSingleRegister(0x286, word(1));
+  Serial.println(" ");
+  Serial.println("RESET MINIMO LT");
+  Serial.println(result);
+  delay(1000);
+
+
+// ESCREVER VALOR DA HYSTERESIS DO COMPRESSOR
+  result = node.writeSingleRegister(0x2811, word(6));
+  Serial.println(" ");
+  Serial.println("HYSTERESIS");
+  Serial.println(result);
+  delay(1000);
+
+
+// // ALTERAR VALOR DA TEMPERATURA MAXIMA DO ALARME
+//   result = node.writeSingleRegister(0x282C, word(270));
+//   Serial.println(" ");
+//   Serial.println("ALARME MAXIMO HT");
+//   Serial.println(result);
+//   delay(1000);
+
+
+// // ALTERAR VALOR DA TEMPERATURA MINIMA DO ALARME
+//   result = node.writeSingleRegister(0x282D, word(40));
+//   Serial.println(" ");
+//   Serial.println("ALARME MINIMO LT");
+//   Serial.println(result);
+//   delay(1000);
 }
